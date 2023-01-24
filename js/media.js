322bce6639cfccbe993c203b29c57af957a7df7e
Giselle <giselle.mingue@gmail.com>
2023-01-24 11:28:29 -0500

    Adding files

---
+++ js/media.js
@@ -0,0 +1,10 @@
+const MEDIA = [
+    {
+    artist: 'Steppenwolf',
+    title: 'Born to be wild',
+    track: 'born-to-be-wild.mp3',
+    thumbnail: 'born-to-be-wild-steppenwolf.png',
+    large: 'born-to-be-wild-steppenwolf.jpg',
+    },
+];
+export default MEDIA;
